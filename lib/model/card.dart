4449d47ec23dae91e9a0bc7271394734d7d04a78
lesiak <lesiak@outlook.com>
2020-01-13 14:45:01 +0100

    Added Card class

---
+++ lib/model/card.dart
@@ -0,0 +1,43 @@
+class Card {
+  String en;
+  String es;
+  String fi;
+  String fr;
+  String it;
+  String de;
+  String dk;
+  String hu;
+  String cs;
+  String ru;
+  String ko;
+
+  factory Card.empty() {
+    return new Card("", "", "", "", "", "", "", "", "", "", "");
+  }
+
+  Card(this.en, this.es, this.fi, this.fr, this.it, this.de, this.dk, this.hu, this.cs, this.ru, this.ko);
+
+  String getValueForLang(String lang) {
+    switch (lang) {
+      case "en": return en;
+      case "fi": return fi;
+      case "fr": return fr;
+      case "es": return es;
+      case "it": return it;
+      case "de": return de;
+      case "de": return dk;
+      case "hu": return hu;
+      case "cs": return cs;
+      case "ru": return ru;
+      case "ko": return ko;
+      default: return null;
+    }
+  }
+
+  @override
+  String toString() {
+    return 'Card{en: $en, es: $es, fi: $fi, fr: $fr, it: $it, de: $de, dk: $dk, hu: $hu, cs: $cs, ru: $ru, ko: $ko}';
+  }
+
+
+}

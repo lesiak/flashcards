7dd6ea08f3e96d2c358839eee87a42d53e03fa21
lesiak <lesiak@outlook.com>
2020-01-14 17:14:25 +0100

    Back to ListView

--- lib/widgets/word_list.dart
+++ lib/widgets/word_list.dart
@@ -8,61 +8,30 @@ class WordList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var count = 1;
-    return SingleChildScrollView(
-      child: Table(
-        //defaultColumnWidth: FixedColumnWidth(150.0),
-        border: TableBorder(
-          horizontalInside: BorderSide(
-            color: Colors.grey,
-            style: BorderStyle.solid,
-            width: 0.4,
-          ),
-          verticalInside: BorderSide(
-            color: Colors.grey,
-            style: BorderStyle.solid,
-            width: 0.4,
-          ),
-        ),
-        children: this.cards.map((c) => _buildTableRow(c)).toList(),
-      ),
-    );
+    return ListView.separated(
+        separatorBuilder: (context, index) => Divider(
+              color: Colors.black,
+            ),
+        itemCount: cards.length,
+        itemBuilder: (BuildContext context, int index) {
+          return buildCard(cards[index], index);
+        });
   }
 
-//  TableRow _buildTableRow(String listOfNames) {
-//    return TableRow(
-//      children: listOfNames.split(',').map((name) {
-//        return Container(
-//          alignment: Alignment.center,
-//          child: Text(name, style: TextStyle(fontSize: 20.0)),
-//          padding: EdgeInsets.all(8.0),
-//        );
-//      }).toList(),
-//    );
-//  }
-
-  TableRow _buildTableRow(FlashCard card) {
-    return TableRow(
-      children: [card.word, card.answer].map((name) {
-        return Container(
-          alignment: Alignment.center,
-          child: Text(name != null ? name : '',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontFamily: 'Roboto',
-              )),
-          padding: EdgeInsets.all(8.0),
-        );
-      }).toList(),
-    );
-  }
-
-  Widget buildCard(FlashCard card) {
+  Widget buildCard(FlashCard card, int index) {
     return Container(
-      child: Column(children: [Text(card.word)]),
-      margin: EdgeInsets.all(5.0),
+      child: Column(
+        children: [
+          Text(card.word,
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w600,
+              )),
+          Text(card.answer, style: TextStyle(fontFamily: 'Roboto'))
+        ],
+        crossAxisAlignment: CrossAxisAlignment.center,
+      ),
       padding: EdgeInsets.all(5.0),
-      decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
     );
   }
 }
